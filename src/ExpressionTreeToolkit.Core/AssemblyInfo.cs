6f6eec508515a386ad6119aec02e00483d18b759
Alessio Gogna <alecsg77@users.noreply.github.com>
2018-07-03 23:27:19 +0200

    Added GitVersion support

---
+++ src/ExpressionTreeToolkit.Core/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ExpressionTreeToolkit.Core")]
+[assembly: ComVisible(false)]
+[assembly: Guid("62e9aaaa-01f5-45e7-b557-ceb88e391750")]
---
+++ src/SolutionInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyDescription("The Expression Tree Toolkit is a collection of helper functions, extension methods, and ready-to-use classes to simplifies common developer tasks manipulating expression trees.")]
+[assembly: AssemblyCompany("Alessio Gogna")]
+[assembly: AssemblyProduct("ExpressionTreeToolkit")]
+[assembly: AssemblyCopyright("Copyright (c) 2018 Alessio Gogna")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
