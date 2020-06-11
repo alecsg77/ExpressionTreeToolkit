4915b0eb52feb480faef4577fea87d9427c0874c
Alessio Gogna <alecsg77@users.noreply.github.com>
2020-06-11 11:25:44 +0200

    Expression Enumerator Initial Draft

---
+++ src/ExpressionTreeToolkit.Core/ExpressionExtensions.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Diagnostics.CodeAnalysis;
+
+#if JETBRAINS_ANNOTATIONS
+using AllowNullAttribute  = JetBrains.Annotations.CanBeNullAttribute;
+using DisallowNullAttribute = JetBrains.Annotations.NotNullAttribute;
+using AllowItemNullAttribute = JetBrains.Annotations.ItemCanBeNullAttribute;
+#endif
+
+namespace ExpressionTreeToolkit
+{
+    public static partial class ExpressionExtensions
+    {
+        public static IEnumerable<Expression> AsEnumerable([DisallowNull] this Expression source)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+
+            return ExpressionIterator(source);
+
+        }
+
+        private static IEnumerable<Expression> ExpressionIterator([DisallowNullAttribute] Expression expression)
+        {
+            if (expression == null)
+            {
+                throw new ArgumentNullException(nameof(expression));
+            }
+
+            switch (expression.NodeType)
+            {
+                case ExpressionType.Negate:
+                case ExpressionType.NegateChecked:
+                case ExpressionType.Not:
+                case ExpressionType.Convert:
+                case ExpressionType.ConvertChecked:
+                case ExpressionType.ArrayLength:
+                case ExpressionType.Quote:
+                case ExpressionType.TypeAs:
+                case ExpressionType.UnaryPlus:
+                case ExpressionType.PreIncrementAssign:
+                case ExpressionType.PreDecrementAssign:
+                case ExpressionType.PostIncrementAssign:
+                case ExpressionType.PostDecrementAssign:
+                case ExpressionType.IsTrue:
+                case ExpressionType.IsFalse:
+                case ExpressionType.OnesComplement:
+                case ExpressionType.Decrement:
+                case ExpressionType.Increment:
+                case ExpressionType.Throw:
+                case ExpressionType.Unbox:
+                    return UnaryIterator((UnaryExpression)expression);
+                case ExpressionType.Add:
+                case ExpressionType.AddChecked:
+                case ExpressionType.Subtract:
+                case ExpressionType.SubtractChecked:
+                case ExpressionType.Multiply:
+                case ExpressionType.MultiplyChecked:
+                case ExpressionType.Divide:
+                case ExpressionType.Modulo:
+                case ExpressionType.And:
+                case ExpressionType.AndAlso:
+                case ExpressionType.Or:
+                case ExpressionType.OrElse:
+                case ExpressionType.LessThan:
+                case ExpressionType.LessThanOrEqual:
+                case ExpressionType.GreaterThan:
+                case ExpressionType.GreaterThanOrEqual:
+                case ExpressionType.Equal:
+                case ExpressionType.NotEqual:
+                case ExpressionType.Coalesce:
+                case ExpressionType.ArrayIndex:
+                case ExpressionType.RightShift:
+                case ExpressionType.LeftShift:
+                case ExpressionType.ExclusiveOr:
+                case ExpressionType.Power:
+                case ExpressionType.Assign:
+                case ExpressionType.AddAssign:
+                case ExpressionType.AndAssign:
+                case ExpressionType.DivideAssign:
+                case ExpressionType.ExclusiveOrAssign:
+                case ExpressionType.LeftShiftAssign:
+                case ExpressionType.ModuloAssign:
+                case ExpressionType.MultiplyAssign:
+                case ExpressionType.OrAssign:
+                case ExpressionType.PowerAssign:
+                case ExpressionType.RightShiftAssign:
+                case ExpressionType.SubtractAssign:
+                case ExpressionType.AddAssignChecked:
+                case ExpressionType.MultiplyAssignChecked:
+                case ExpressionType.SubtractAssignChecked:
+                    return BinaryIterator((BinaryExpression)expression);
+                case ExpressionType.TypeIs:
+                case ExpressionType.TypeEqual:
+                    return TypeBinaryIterator((TypeBinaryExpression)expression);
+                case ExpressionType.Conditional:
+                    return ConditionalIterator((ConditionalExpression)expression);
+                case ExpressionType.Constant:
+                    return ConstantIterator((ConstantExpression)expression);
+                case ExpressionType.Parameter:
+                    return ParameterIterator((ParameterExpression)expression);
+                case ExpressionType.MemberAccess:
+                    return MemberIterator((MemberExpression)expression);
+                case ExpressionType.Call:
+                    return MethodCallIterator((MethodCallExpression)expression);
+                case ExpressionType.Lambda:
+                    return LambdaIterator((LambdaExpression)expression);
+                case ExpressionType.New:
+                    return NewIterator((NewExpression)expression);
+                case ExpressionType.NewArrayInit:
+                case ExpressionType.NewArrayBounds:
+                    return NewArrayIterator((NewArrayExpression)expression);
+                case ExpressionType.Invoke:
+                    return InvocationIterator((InvocationExpression)expression);
+                case ExpressionType.MemberInit:
+                    return MemberInitIterator((MemberInitExpression)expression);
+                case ExpressionType.ListInit:
+                    return ListInitIterator((ListInitExpression)expression);
+                case ExpressionType.Block:
+                    return BlockIterator((BlockExpression)expression);
+                case ExpressionType.DebugInfo:
+                    return DebugInfoIterator((DebugInfoExpression)expression);
+                case ExpressionType.Dynamic:
+                    return DynamicIterator((DynamicExpression)expression);
+                case ExpressionType.Goto:
+                    return GotoIterator((GotoExpression)expression);
+                case ExpressionType.Index:
+                    return IndexIterator((IndexExpression)expression);
+                case ExpressionType.Label:
+                    return LabelIterator((LabelExpression)expression);
+                case ExpressionType.RuntimeVariables:
+                    return RuntimeVariablesIterator((RuntimeVariablesExpression)expression);
+                case ExpressionType.Loop:
+                    return LoopIterator((LoopExpression)expression);
+                case ExpressionType.Switch:
+                    return SwitchIterator((SwitchExpression)expression);
+                case ExpressionType.Try:
+                    return TryIterator((TryExpression)expression);
+                case ExpressionType.Default:
+                    return DefaultIterator((DefaultExpression)expression);
+                case ExpressionType.Extension:
+                    return ExtensionIterator(expression);
+                default:
+                    return null;
+            }
+        }
+
+        private static IEnumerable<Expression> UnaryIterator(UnaryExpression expression)
+        {
+            yield return expression.Operand;
+            yield return expression;
+        }
+
+        private static IEnumerable<Expression> BinaryIterator(BinaryExpression expression)
+        {
+            yield return expression.Left;
+            yield return expression.Right;
+            yield return expression;
+        }
+
+        private static IEnumerable<Expression> TypeBinaryIterator(TypeBinaryExpression expression)
+        {
+            yield return expression.Expression;
+            yield return expression;
+        }
+
+        private static IEnumerable<Expression> ConditionalIterator(ConditionalExpression expression)
+        {
+            yield return expression.Test;
+            yield return expression.IfTrue;
+            yield return expression.IfFalse;
+            yield return expression;
+        }
+
+        private static IEnumerable<Expression> ConstantIterator(ConstantExpression expression)
+        {
+            yield return expression;
+        }
+
+        private static IEnumerable<Expression> ParameterIterator(ParameterExpression expression)
+        {
+            yield return expression;
+        }
+
+        private static IEnumerable<Expression> MemberIterator(MemberExpression expression)
+        {
+            if (expression.Expression != null) yield return expression.Expression;
+            yield return expression;
+        }
+
+        private static IEnumerable<Expression> MethodCallIterator(MethodCallExpression expression)
+        {
+            if (expression.Object != null) yield return expression.Object;
+            foreach (var argument in expression.Arguments)
+            {
+                yield return argument;
+            }
+            yield return expression;
+        }
+
+        private static IEnumerable<Expression> LambdaIterator(LambdaExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static IEnumerable<Expression> NewIterator(NewExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static IEnumerable<Expression> NewArrayIterator(NewArrayExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static IEnumerable<Expression> InvocationIterator(InvocationExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static IEnumerable<Expression> MemberInitIterator(MemberInitExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static IEnumerable<Expression> ListInitIterator(ListInitExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static IEnumerable<Expression> BlockIterator(BlockExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static IEnumerable<Expression> DebugInfoIterator(DebugInfoExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static IEnumerable<Expression> DynamicIterator(DynamicExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static IEnumerable<Expression> GotoIterator(GotoExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static IEnumerable<Expression> IndexIterator(IndexExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static IEnumerable<Expression> LabelIterator(LabelExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static IEnumerable<Expression> RuntimeVariablesIterator(RuntimeVariablesExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static IEnumerable<Expression> LoopIterator(LoopExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static IEnumerable<Expression> SwitchIterator(SwitchExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static IEnumerable<Expression> TryIterator(TryExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static IEnumerable<Expression> DefaultIterator(DefaultExpression expression)
+        {
+            yield return expression;
+        }
+
+        private static IEnumerable<Expression> ExtensionIterator(Expression expression)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
---
+++ test/ExpressionTreeToolkit.UnitTests/ExpressionEnumeratorBehaviour.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using Xunit;
+
+namespace ExpressionTreeToolkit.UnitTests
+{
+    public class ExpressionEnumeratorBehaviour
+    {
+        [Fact]
+        public void ShouldThrowArgumentNullExceptionOnNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => ExpressionExtensions.AsEnumerable(null));
+        }
+
+        [Fact]
+        public void ShouldReturnEnumerableOnExpression()
+        {
+            var source = Expression.Empty();
+            var actual = ExpressionExtensions.AsEnumerable(source);
+            Assert.IsAssignableFrom<IEnumerable<Expression>>(actual);
+        }
+
+        [Fact]
+        public void ShouldMoveNextOnExpression()
+        {
+            var source = Expression.Empty();
+            using (var actual = ExpressionExtensions.AsEnumerable(source).GetEnumerator())
+            {
+                Assert.True(actual.MoveNext());
+            }
+        }
+
+        [Fact]
+        public void ShouldCurrentBeTheSameAsSource()
+        {
+            var source = Expression.Empty();
+            using (var actual = ExpressionExtensions.AsEnumerable(source).GetEnumerator())
+            {
+                actual.MoveNext();
+                Assert.Same(source, actual.Current);
+            }
+        }
+
+        [Fact]
+        public void ShouldEnumerateUnaryExpressionAs_Operand_Node()
+        {
+            var operand = Expression.Default(typeof(bool));
+            var node = Expression.Not(operand);
+            var expected = new Expression[] { operand, node };
+
+            var actual = ExpressionExtensions.AsEnumerable(node);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ShouldEnumerateBinaryExpressionAs_Left_Right_Node()
+        {
+            var left = Expression.Default(typeof(bool));
+            var right = Expression.Default(typeof(bool));
+            var node = Expression.And(left, right);
+            var expected = new Expression[] { left, right, node };
+
+            var actual = ExpressionExtensions.AsEnumerable(node);
+
+            Assert.Equal(expected, actual);
+        }
+
+
+        [Fact]
+        public void ShouldEnumerateTypeBinaryExpressionAs_Expression_Node()
+        {
+            var expression = Expression.Default(typeof(bool));
+            var node = Expression.TypeEqual(expression, typeof(int));
+            var expected = new Expression[] { expression, node };
+
+            var actual = ExpressionExtensions.AsEnumerable(node);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ShouldEnumerateConditionalExpressionAs_Test_IfTrue_IfFalse_Node()
+        {
+            var test = Expression.Default(typeof(bool));
+            var ifTrue = Expression.Default(typeof(int));
+            var ifFalse = Expression.Default(typeof(int));
+            var node = Expression.Condition(test, ifTrue, ifFalse);
+            var expected = new Expression[] { test, ifTrue, ifFalse, node };
+
+            var actual = ExpressionExtensions.AsEnumerable(node);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ShouldEnumerateConstantExpressionAs_TNode()
+        {
+            var node = Expression.Constant(0);
+            var expected = new Expression[] { node };
+
+            var actual = ExpressionExtensions.AsEnumerable(node);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ShouldEnumerateParameterExpressionAs_TNode()
+        {
+            var node = Expression.Parameter(typeof(int));
+            var expected = new Expression[] { node };
+
+            var actual = ExpressionExtensions.AsEnumerable(node);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ShouldEnumerateMemberExpressionAs_Node()
+        {
+            var memberInfo = typeof(int).GetField(nameof(int.MinValue));
+            var node = Expression.MakeMemberAccess(null, memberInfo);
+            var expected = new Expression[] { node };
+
+            var actual = ExpressionExtensions.AsEnumerable(node);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ShouldEnumerateMemberExpressionAs_Expression_Node()
+        {
+            var expression = Expression.Default(typeof(Expression));
+            var propertyInfo = typeof(Expression).GetProperty(nameof(Expression.NodeType));
+            var node = Expression.MakeMemberAccess(expression, propertyInfo);
+            var expected = new Expression[] { expression, node };
+
+            var actual = ExpressionExtensions.AsEnumerable(node);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ShouldEnumerateMethodCallExpressionAs_Node()
+        {
+            var methodInfo = typeof(Expression).GetMethod(nameof(Expression.Empty));
+            var node = Expression.Call(null, methodInfo);
+            var expected = new Expression[] { node };
+
+            var actual = ExpressionExtensions.AsEnumerable(node);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ShouldEnumerateMethodCallExpressionAs_Object_Node()
+        {
+            var @object = Expression.Default(typeof(Expression));
+            var methodInfo = typeof(Expression).GetMethod(nameof(Expression.Reduce));
+            var node = Expression.Call(@object, methodInfo);
+            var expected = new Expression[] { @object, node };
+
+            var actual = ExpressionExtensions.AsEnumerable(node);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ShouldEnumerateMethodCallExpressionAs_Args_Node()
+        {
+            var methodInfo = typeof(Expression).GetMethod(nameof(Expression.ReferenceEqual));
+            var arg0 = Expression.Default(typeof(Expression));
+            var arg1 = Expression.Default(typeof(Expression));
+            var node = Expression.Call(null, methodInfo, arg0, arg1);
+            var expected = new Expression[] { arg0, arg1, node };
+
+            var actual = ExpressionExtensions.AsEnumerable(node);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ShouldEnumerateMethodCallExpressionAs_Object_Args_Node()
+        {
+            var @object = Expression.Default(typeof(Expression));
+            var methodInfo = typeof(Expression).GetMethod(nameof(Expression.Equals));
+            var arg0 = Expression.Default(typeof(Expression));
+            var node = Expression.Call(@object, methodInfo, arg0);
+            var expected = new Expression[] { @object, arg0, node };
+
+            var actual = ExpressionExtensions.AsEnumerable(node);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+
+}
